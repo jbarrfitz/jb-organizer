44b70f199a02e4bbdb5dfad39c5461b064f0486a
rykeller.tech@gmail.com <rykeller.tech@gmail.com>
2022-08-28 13:53:32 -0500

    fix: environment mismatch

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // env: {
+  //   FIREBASE_PUBLIC_API_KEY: true,
+  //   FIREBASE_PUBLIC_AUTH_DOMAIN: true,
+  //   FIREBASE_PUBLIC_PROJECT_ID: true,
+  //   FIREBASE_PUBLIC_STORAGE_BUCKET: true,
+  //   FIREBASE_PUBLIC_MESSAGING_SENDER_ID: true,
+  //   FIREBASE_PUBLIC_APP_ID: true,
+  //   FIREBASE_PUBLIC_MEASUREMENT_ID: true,
+  // }
 }
 
 module.exports = nextConfig
