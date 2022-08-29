4fa00532272bc80fa24d774cb8fd588f920015ee
rykeller.tech@gmail.com <rykeller.tech@gmail.com>
2022-08-28 21:43:03 -0500

    fix: bug with serverless check built into twilio

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,11 +1,2 @@
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
-
-beforeAll(() => {
-    jest.setSystemTime(new Date(2022, 2, 1, 9, 30, 0, 0));
-    // Default: 3/1/22 @9:30am
-});
-
-afterAll(() => {
-    jest.useRealTimers();
-});
