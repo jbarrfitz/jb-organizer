36dd698e14aa2f6acb0f42486b433af63b2c7cc3
jerry.barrows@gmail.com <jerry.barrows@gmail.com>
2022-09-25 13:03:34 -0500

    feat: api

--- firebase/server.ts
+++ firebase/server.ts
@@ -4,22 +4,15 @@ import * as admin from 'firebase-admin'
 const serviceAccount = require('../firebase-service-account.json')
 
 // Initialize the app with a service account, granting admin privileges
-// if (!admin.apps.length) {
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  // The database URL depends on the location of the database
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL
-});
-
-// if (!admin.apps.length) {
-//   admin.initializeApp({
-//     credential: admin.credential.cert({
-//       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-//       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-//     }),
-//     databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-//   })
-// }
+if (!admin.apps.length) {
+    admin.initializeApp({
+        credential: admin.credential.cert({
+            projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        }),
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL
+    });
+}
 
 export default admin;
--- pages/api/[gameDate].ts
+++ pages/api/[gameDate].ts
@@ -10,10 +10,31 @@
 // }
 import { NextApiRequest, NextApiResponse } from 'next';
 import admin from '../../firebase/server'
+import { IDatabase, IGame } from '../../types';
+
+const initialGameState: IGame = {
+  config: {
+    rounds: 5
+  },
+  rounds: [],
+  teams: {}
+}
+
+const dateId = (dateString: Date | string = new Date()) => {
+  return new Date(dateString).toLocaleDateString().replaceAll('/', '-')
+}
 
 export default async function firebaseApi(req: NextApiRequest, res: NextApiResponse) {
+  console.warn
   const db = admin.database()
-  const ref = db.ref('secret')
-  const data = await ref.once('value')
-  res.status(200).json({data});
+  const dateString = dateId('9/25/2022')
+  const ref = db.ref(`games`)
+  // const ref = db.ref(`games/${dateString}`)
+  let game = await ref.once('value')
+  if (!game) {
+    await ref.set(initialGameState)
+    console.warn('SET STATE')
+    res.status(201).json({ game: initialGameState});
+  }
+  res.status(200).json({game});
 }
